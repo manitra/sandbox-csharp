af7000caa70fdb5f5b7b06fdca1169bf5a60386c
Manitra Andriamitondra <manitra@manitra.net>
2015-12-31 11:48:05 +0700

    Create GetAttribute tests

---
+++ Sandbox.Console/GetAttributeTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet;
+using NUnit.Framework;
+
+namespace Sandbox.Console
+{
+    [TestFixture]
+    public class GetAttributeTest
+    {
+        [Test]
+        public void GetAttributeWithInheritance_ReturnsSomething()
+        {
+            Assert.IsNotEmpty(typeof(Child).GetCustomAttributes(typeof(Attr), true));
+        }
+        
+        [Test]
+        public void GetAttributeWithoutInheritance_ReturnsNull()
+        {
+            Assert.IsEmpty(typeof(Child).GetCustomAttributes(typeof(Attr), false));
+        }
+        
+        [Attr]
+        public class Parent
+        {
+
+        }
+
+        public class Child : Parent
+        {
+
+        }
+
+        public class Attr : Attribute
+        {
+
+        }
+    }
+
+}
